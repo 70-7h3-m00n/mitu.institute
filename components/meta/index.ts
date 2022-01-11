ada6add9567f8d9595f675d5308463bfda05c8a7
novailoveyou <novailoveyoux@gmail.com>
2022-01-11 22:24:07 +0300

    improved structure & added meta components

---
+++ components/meta/index.ts
@@ -0,0 +1,5 @@
+import MetaIcons from '@/components/meta/MetaIcons'
+import MetaOg from '@/components/meta/MetaOg'
+import MetaTwitter from '@/components/meta/MetaTwitter'
+
+export { MetaIcons, MetaOg, MetaTwitter }
