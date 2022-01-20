2b3c2fc5ddadf54adaa80ed64fb328ea233cac49
Novailoveyou <novailoveyoux@gmail.com>
2022-01-20 12:45:11 +0300

    improved icons structure

--- components/icons/index.ts
+++ components/icons/index.ts
@@ -6,6 +6,8 @@ import IconClock from '@/components/icons/IconClock'
 import IconCoatOfArmsRussia from '@/components/icons/IconCoatOfArmsRussia'
 import IconCross from '@/components/icons/IconCross'
 import IconCrossAlt from '@/components/icons/IconCrossAlt'
+import IconFacebook from '@/components/icons/IconFacebook'
+import IconInstagram from '@/components/icons/IconInstagram'
 import IconLightBulb from '@/components/icons/IconLightBulb'
 import IconLoader from '@/components/icons/IconLoader'
 import IconLocation from '@/components/icons/IconLocation'
@@ -15,10 +17,8 @@ import IconMortarboardAlt from '@/components/icons/IconMortarboardAlt'
 import IconPaperPage from '@/components/icons/IconPaperPage'
 import IconPhone from '@/components/icons/IconPhone'
 import IconVideoOnStand from '@/components/icons/IconVideoOnStand'
-import IconInstagram from './IconInstagram'
-import IconVK from './IconVK'
-import IconFacebook from './IconFacebook'
-import IconYoutube from './IconYoutube'
+import IconVK from '@/components/icons/IconVK'
+import IconYoutube from '@/components/icons/IconYoutube'
 
 export {
   IconArrowTopRight,
@@ -29,6 +29,8 @@ export {
   IconCoatOfArmsRussia,
   IconCross,
   IconCrossAlt,
+  IconInstagram,
+  IconFacebook,
   IconLightBulb,
   IconLoader,
   IconLocation,
@@ -38,8 +40,6 @@ export {
   IconPaperPage,
   IconPhone,
   IconVideoOnStand,
-  IconInstagram,
   IconVK,
-  IconFacebook,
   IconYoutube
 }
