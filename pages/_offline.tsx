1420fbee3fc62394c27a11f700727c4e4477a9f1
Novailoveyou <novailoveyoux@gmail.com>
2022-01-21 17:52:14 +0300

    styles offline page

--- pages/_offline.tsx
+++ pages/_offline.tsx
@@ -1,15 +1,22 @@
+import stls from '@/styles/pages/PageOffine.module.sass'
 import { phoneNumber } from '@/config/index'
+import { Wrapper } from '@/components/layout'
 
-const _offline = () => {
+const PageOffline = () => {
   return (
-    <div>
-      <p>
-        Подключение к сети отсутствует. Пожалуйста, попробуйте позже или
-        позвоните нам по номеру
-        <a href={phoneNumber.href}>{phoneNumber.val}</a>
-      </p>
-    </div>
+    <>
+      <Wrapper>
+        <div className={stls.content}>
+          <h1 className={stls.title}>Zzz...</h1>
+          <p className={stls.p}>
+            Подключение к сети отсутствует. Пожалуйста, попробуйте позже или
+            позвоните нам по номеру{' '}
+            <a href={phoneNumber.href}>{phoneNumber.val}</a>
+          </p>
+        </div>
+      </Wrapper>
+    </>
   )
 }
 
-export default _offline
+export default PageOffline
