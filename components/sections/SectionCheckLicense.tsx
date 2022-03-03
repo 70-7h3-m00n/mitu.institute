a34dbbad44c51afda8fbe0913adc265dd2d0014c
Novailoveyou <novailoveyoux@gmail.com>
2022-03-03 09:21:51 +0300

    updated styles & layout for SectionCheckLicense

--- components/sections/SectionCheckLicense.tsx
+++ components/sections/SectionCheckLicense.tsx
@@ -11,65 +11,23 @@ import { BtnBeta } from '@/components/btns'
 type TypeSectionCheckLicenseProps = TypeClassNames
 
 const SectionCheckLicense = ({ classNames }: TypeSectionCheckLicenseProps) => {
-  const testimonials = [
-    {
-      title: '10 лет',
-      subtitle: 'Занимаемся образованием'
-    },
-    {
-      title: '>2000',
-      subtitle: 'Учатся прямо сейчас'
-    },
-    {
-      title: '7 лет',
-      subtitle: mituinstitute
-        ? 'Специализируемся на дистанционном обучении'
-        : 'Специализируемся на дистанционном обучении'
-    },
-    {
-      title: '>10 000',
-      subtitle: 'Студентов уже обучили'
-    }
-  ]
-
   return (
     <section
       className={
         cn(stls.container, getClassNames({ classNames })) || undefined
       }>
       <Wrapper>
-        <GeneralSectionTitle>Проверьте лицензию</GeneralSectionTitle>
-        <div className={stls.content}>
-          <div className={stls.top}>
-            <div className={stls.left}>
-              <div className={stls.pgroup}>
-                <p className={cn(stls.p, stls.pTop)}>
-                  Наш университет за современный подход в образовании. Мы
-                  постоянно берем обратную связь от работодателей и адаптируем
-                  учебные программы
-                </p>
-                <p className={cn(stls.p, stls.pBottom)}>
-                  <GeneralTextHighlight alt>Наша миссия</GeneralTextHighlight> —
-                  создать систему образования, которая будет доступна каждому.
-                  Дистанционное обучение полностью отвечает поставленным целям и
-                  задачам, поэтому инновационные технологии — неотъемлемая часть
-                  образовательного процесса в нашем институте
-                </p>
-              </div>
-              <div className={cn(stls.btns)}>
-                <BtnBeta
-                  variant='alpha'
-                  classNames={[stls.btn, stls.btnLeft]}
-                  tag='a'
-                  href={routesExternal.license}
-                  target='_blank'>
-                  Государственная лицензия на право ведения образовательной
-                  деятельности
-                </BtnBeta>
-              </div>
-            </div>
-          </div>
-        </div>
+        <GeneralSectionTitle classNames={[stls.title]}>
+          Проверьте лицензию
+        </GeneralSectionTitle>
+        <BtnBeta
+          variant='alpha'
+          classNames={[stls.btn]}
+          tag='a'
+          href={routesExternal.license}
+          target='_blank'>
+          Государственная лицензия на право ведения образовательной деятельности
+        </BtnBeta>
       </Wrapper>
     </section>
   )
