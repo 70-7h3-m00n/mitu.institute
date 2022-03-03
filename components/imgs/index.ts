89b65a9c4c8a2145a00cb157ac688bd517518942
Novailoveyou <novailoveyoux@gmail.com>
2022-03-03 11:05:54 +0300

    made img export index file easier to work with

--- components/imgs/index.ts
+++ components/imgs/index.ts
@@ -1,40 +1,19 @@
 // diplomas
-import ImgDiploma1 from '@/components/imgs/diplomas/ImgDiploma1'
-import ImgDiploma2 from '@/components/imgs/diplomas/ImgDiploma2'
-import ImgDiploma3 from '@/components/imgs/diplomas/ImgDiploma3'
+export { default as ImgDiploma1 } from '@/components/imgs/diplomas/ImgDiploma1'
+export { default as ImgDiploma2 } from '@/components/imgs/diplomas/ImgDiploma2'
+export { default as ImgDiploma3 } from '@/components/imgs/diplomas/ImgDiploma3'
 
 // general
-import ImgForWhom from '@/components/imgs/general/ImgForWhom'
-import ImgHeroLady from '@/components/imgs/general/ImgHeroLady'
-import ImgJobTitle from '@/components/imgs/general/ImgJobTitle'
-import ImgPersonStudying from '@/components/imgs/general/ImgPersonStudying'
-import ImgProgramContents from '@/components/imgs/general/ImgProgramContents'
-import ImgProgramHero from '@/components/imgs/general/ImgProgramHero'
-import ImgStudyCost from '@/components/imgs/general/ImgStudyCost'
-import ImgTeacher from '@/components/imgs/general/ImgTeacher'
-import ImgWhyUs from '@/components/imgs/general/ImgWhyUs'
+export { default as ImgForWhom } from '@/components/imgs/general/ImgForWhom'
+export { default as ImgHeroLady } from '@/components/imgs/general/ImgHeroLady'
+export { default as ImgJobTitle } from '@/components/imgs/general/ImgJobTitle'
+export { default as ImgPersonStudying } from '@/components/imgs/general/ImgPersonStudying'
+export { default as ImgProgramContents } from '@/components/imgs/general/ImgProgramContents'
+export { default as ImgProgramHero } from '@/components/imgs/general/ImgProgramHero'
+export { default as ImgStudyCost } from '@/components/imgs/general/ImgStudyCost'
+export { default as ImgTeacher } from '@/components/imgs/general/ImgTeacher'
+export { default as ImgWhyUs } from '@/components/imgs/general/ImgWhyUs'
 
 // icons
-import ImgLogo from '@/components/imgs/icons/ImgLogo'
-
-import ImgTemplate from '@/components/imgs/ImgTemplate'
-
-export {
-  // diplomas
-  ImgDiploma1,
-  ImgDiploma2,
-  ImgDiploma3,
-  // general
-  ImgForWhom,
-  ImgHeroLady,
-  ImgJobTitle,
-  ImgPersonStudying,
-  ImgProgramContents,
-  ImgProgramHero,
-  ImgStudyCost,
-  ImgTeacher,
-  ImgWhyUs,
-  // icons
-  ImgLogo,
-  ImgTemplate
-}
+export { default as ImgLogo } from '@/components/imgs/icons/ImgLogo'
+export { default as ImgTemplate } from '@/components/imgs/ImgTemplate'
