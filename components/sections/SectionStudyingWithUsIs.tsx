0f6a6f7ecf7cd5702a8318e75c317b7916caed3b
Novailoveyou <novailoveyoux@gmail.com>
2022-03-09 20:08:33 +0300

    minor changes

--- components/sections/SectionStudyingWithUsIs.tsx
+++ components/sections/SectionStudyingWithUsIs.tsx
@@ -2,17 +2,40 @@ import stls from '@/styles/components/sections/SectionStudyingWithUsIs.module.sa
 import { TypeClassNames } from '@/types/index'
 import { useContext } from 'react'
 import cn from 'classnames'
-import { studyingWithUsIs } from '@/data/index'
+// import { studyingWithUsIs } from '@/data/index'
 import { getClassNames } from '@/helpers/index'
-import { ContextProgramContext } from '@/context/index'
+import { ContextCategoriesContext } from '@/context/index'
 import { Wrapper } from '@/components/layout'
 import { GeneralSectionTitle, GeneralTextHighlight } from '@/components/general'
+
+type TypeStudyingWithUsIs = string[]
 
 type TypeSectionStudyingWithUsIsProps = TypeClassNames
 
 const SectionStudyingWithUsIs = ({
   classNames
 }: TypeSectionStudyingWithUsIsProps) => {
+  const { curCategory } = useContext(ContextCategoriesContext)
+
+  const studyingWithUsIs: TypeStudyingWithUsIs =
+    curCategory?.type === 'bachelor' || curCategory?.type === 'master'
+      ? [
+          'Дистанционное обучение. Можете обучаться из любой точки мира',
+          // 'Отсрочка от армии',
+          'Образовательные программы, адаптированные под требования работодателей',
+          'Поддержка куратора 24/7',
+          'Возможность вернуть 13% от стоимости обучения',
+          'Перевод из других ВУЗов без потери курса'
+        ]
+      : [
+          'Дистанционное обучение. Можете обучаться из любой точки мира',
+          // 'Отсрочка от армии',
+          'Образовательные программы, адаптированные под требования работодателей',
+          'Поддержка куратора 24/7',
+          'Возможность вернуть 13% от стоимости обучения'
+          // 'Перевод из других ВУЗов без потери курса'
+        ]
+
   return (
     <section
       className={
--- data/index.ts
+++ data/index.ts
@@ -1,4 +1,3 @@
 export { default as leastDocuments } from '@/data/general/leastDocuments'
 export { default as pros } from '@/data/general/pros'
-export { default as studyingWithUsIs } from '@/data/general/studyingWithUsIs'
 export { default as whyus } from '@/data/general/whyus'
