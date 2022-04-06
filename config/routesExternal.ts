14d9c43df77ce15af9b4bc9d7a4360f204ff80af
Novailoveyou <novailoveyoux@gmail.com>
2022-04-06 12:26:19 +0300

    updated payment link

--- config/routesExternal.ts
+++ config/routesExternal.ts
@@ -1,12 +1,12 @@
 type TypeRoutesExternal = {
   license: 'https://islod.obrnadzor.gov.ru/rlic/details/0B100E0A-0A0B-110B-0D10-0E13110C0C111010110E/'
-  payment: 'https://3dsec.sberbank.ru/shortlink/9jYQOrNj'
+  payment: 'https://securepayments.sberbank.ru/shortlink/vdGUaCf3'
 }
 
 const routesExternal: TypeRoutesExternal = {
   license:
     'https://islod.obrnadzor.gov.ru/rlic/details/0B100E0A-0A0B-110B-0D10-0E13110C0C111010110E/',
-  payment: 'https://3dsec.sberbank.ru/shortlink/9jYQOrNj'
+  payment: 'https://securepayments.sberbank.ru/shortlink/vdGUaCf3'
 }
 
 export default routesExternal
