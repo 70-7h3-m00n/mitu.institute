488fe500ee3718b9665a73ccbc023fade26dc9e2
novailoveyou <novailoveyoux@gmail.com>
2022-05-24 13:00:12 +0300

    set revalidate to 60 sec

--- config/revalidate.ts
+++ config/revalidate.ts
@@ -3,7 +3,8 @@ import { mituinstitute } from '@/config/index'
 const revalidate = {
   // default: 60 * 60 * 24 * 7,
   // default: mituinstitute ? 60 : false,
-  default: false,
+  // default: false,
+  default: 60,
   day: 60 * 60 * 24,
   hour: 60 * 60,
   minute: 60,
