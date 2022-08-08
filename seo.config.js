8ebd3fdaec113e553caa4c956cb7c2c7875f2c83
novailoveyou <novailoveyoux@gmail.com>
2022-08-08 14:09:03 +0300

    removed noindex & nofollow for mituinstitute

--- seo.config.js
+++ seo.config.js
@@ -32,11 +32,11 @@ const seo = {
   // },
   defaultTitle: seoParams.title,
   description: seoParams.desc,
-  canonical: routesFront.officialRoot,
-  dangerouslySetAllPagesToNoIndex: mituinstitute,
-  dangerouslySetAllPagesToNoFollow: mituinstitute,
-  noindex: mituinstitute,
-  nofollow: mituinstitute
+  canonical: routesFront.officialRoot
+  // dangerouslySetAllPagesToNoIndex: mituinstitute,
+  // dangerouslySetAllPagesToNoFollow: mituinstitute,
+  // noindex: mituinstitute,
+  // nofollow: mituinstitute
 }
 
 export default seo
