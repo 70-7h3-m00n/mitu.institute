7062ea5646b9b52eacb164ed7341d7eacfe5d281
novailoveyou <novailoveyoux@gmail.com>
2022-08-08 22:20:45 +0300

    set mituinstitute to true

--- config/mituinstitute.ts
+++ config/mituinstitute.ts
@@ -1,6 +1,7 @@
 import { routesFront } from '@/config/index'
 
 // const mituinstitute = !(routesFront.root === routesFront.defaultRoot)
-const mituinstitute = routesFront.root === routesFront.defaultRoot
+// const mituinstitute = routesFront.root === routesFront.defaultRoot
+const mituinstitute = true
 
 export default mituinstitute
