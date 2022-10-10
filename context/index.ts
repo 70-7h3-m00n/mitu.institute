0b5bdb20f4bac31d83f64b09ad08d5b399a238f9
BaurinAnton <baurinanton2013@yandex.ru>
2022-10-10 12:47:00 +0300

    removed unnecessary global useReduce statuses

--- context/index.ts
+++ context/index.ts
@@ -1,45 +1,25 @@
+import ContextAccessibilityState from '@/context/accessibility/ContextAccessibilityState'
 import ContextAccessibilityContext from '@/context/accessibility/contextAccessibilityContext'
 import contextAccessibilityReducer from '@/context/accessibility/contextAccessibilityReducer'
-import ContextAccessibilityState from '@/context/accessibility/ContextAccessibilityState'
-import ContextCategoriesContext from '@/context/categories/contextCategoriesContext'
-import contextCategoriesReducer from '@/context/categories/contextCategoriesReducer'
-import ContextCategoriesState from '@/context/categories/ContextCategoriesState'
+import ContextGeneralPopupState from '@/context/popup/ContextGeneralPopupState'
 import ContextGeneralPopupContext from '@/context/popup/contextGeneralPopupContext'
 import contextGeneralPopupReducer from '@/context/popup/contextGeneralPopupReducer'
-import ContextGeneralPopupState from '@/context/popup/ContextGeneralPopupState'
+import ContextCategoriesContext from '@/context/categories/contextCategoriesContext'
 import ContextProgramContext from '@/context/program/contextProgramContext'
-import contextProgramReducer from '@/context/program/contextProgramReducer'
-import ContextProgramState from '@/context/program/ContextProgramState'
 import ContextProgramsContext from '@/context/programs/contextProgramsContext'
-import contextProgramsReducer from '@/context/programs/contextProgramsReducer'
-import ContextProgramsState from '@/context/programs/ContextProgramsState'
 import ContextQuestionsContext from '@/context/questions/contextQuestionsContext'
-import contextQuestionsReducer from '@/context/questions/contextQuestionsReducer'
-import ContextQuestionsState from '@/context/questions/ContextQuestionsState'
 import ContextStudyFieldContext from '@/context/studyField/contextStudyFieldContext'
-import contextStudyFieldReducer from '@/context/studyField/contextStudyFieldReducer'
-import ContextStudyFieldState from '@/context/studyField/ContextStudyFieldState'
 
 export {
   ContextAccessibilityContext,
   contextAccessibilityReducer,
   ContextAccessibilityState,
-  ContextCategoriesContext,
-  contextCategoriesReducer,
-  ContextCategoriesState,
   ContextGeneralPopupContext,
   contextGeneralPopupReducer,
   ContextGeneralPopupState,
+  ContextCategoriesContext,
   ContextProgramContext,
-  contextProgramReducer,
-  ContextProgramState,
   ContextProgramsContext,
-  contextProgramsReducer,
-  ContextProgramsState,
   ContextQuestionsContext,
-  contextQuestionsReducer,
-  ContextQuestionsState,
   ContextStudyFieldContext,
-  contextStudyFieldReducer,
-  ContextStudyFieldState
 }
