7d021fad46411a8cd2d03468c60a2ae9c71c3493
roman <romandreevich@icloud.com>
2023-05-24 16:31:55 +0300

    feat: roistat integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -182,6 +182,17 @@ const App = ({ Component, pageProps, router }: AppProps) => {
           }}
         />
       )}
+        <Script
+            id='roistat-counter'
+            dangerouslySetInnerHTML={{
+                __html: `(function(w, d, s, h, id) {
+                w.roistatProjectId = id; w.roistatHost = h;
+                var p = d.location.protocol == "https:" ? "https://" : "http://";
+                var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init?referrer="+encodeURIComponent(d.location.href);
+                var js = d.createElement(s); js.charset="UTF-8"; js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);
+                })(window, document, 'script', 'cloud.roistat.com', '46751d07945ec38697681458052a865d');`
+            }}
+        />
     </>
   )
 }
